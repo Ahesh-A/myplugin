3df73835d5b9ddf1f836493dd20eaae6f25bdb0b
Ahesh-A <aheshalagu@gmail.com>
2024-05-24 18:56:37 +0530

    defined class MyConfig

--- src/main/java/com/ahesh/vault/config/MyConfig.java
+++ src/main/java/com/ahesh/vault/config/MyConfig.java
@@ -1,9 +1,15 @@
 package com.ahesh.vault.config;
 
+import static hudson.Util.fixEmptyAndTrim;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
+import hudson.model.Descriptor;
 import java.io.Serializable;
 import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 public class MyConfig extends AbstractDescribableImpl<MyConfig> implements Serializable {
 
@@ -11,11 +17,38 @@ public class MyConfig extends AbstractDescribableImpl<MyConfig> implements Seria
     private StringCredentials password;
 
     @DataBoundConstructor
-    public MyConfig(){}
+    public MyConfig() {}
 
     public MyConfig(MyConfig config) {
         this.name = config.name;
         this.password = config.password;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    @DataBoundSetter
+    public void setName(String name) {
+        this.name = fixEmptyAndTrim(name);
+    }
+
+    public StringCredentials getPassword() {
+        return password;
+    }
+
+    @DataBoundSetter
+    public void setPassword(StringCredentials password) {
+        this.password = password;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends Descriptor<MyConfig> {
+
+        @Override
+        @NonNull
+        public String getDisplayName() {
+            return "My Config";
+        }
+    }
 }
