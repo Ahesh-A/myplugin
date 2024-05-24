47075b96bcd3388246ce49ecc857c4c97e18ab8f
Ahesh-A <aheshalagu@gmail.com>
2024-05-24 18:22:02 +0530

    added constuctor and parametrized constructor

--- src/main/java/com/ahesh/vault/config/MyConfig.java
+++ src/main/java/com/ahesh/vault/config/MyConfig.java
@@ -2,7 +2,20 @@ package com.ahesh.vault.config;
 
 import hudson.model.AbstractDescribableImpl;
 import java.io.Serializable;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 public class MyConfig extends AbstractDescribableImpl<MyConfig> implements Serializable {
+
     private String name;
+    private StringCredentials password;
+
+    @DataBoundConstructor
+    public MyConfig(){}
+
+    public MyConfig(MyConfig config) {
+        this.name = config.name;
+        this.password = config.password;
+    }
+
 }
