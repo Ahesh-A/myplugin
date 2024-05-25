dd3a1566c68f4cbe37c74b95da9b2bd2b9e24a73
Ahesh-A <aheshalagu@gmail.com>
2024-05-25 18:24:43 +0530

    added function doFillPasswordItems

--- src/main/java/com/ahesh/vault/config/MyConfig.java
+++ src/main/java/com/ahesh/vault/config/MyConfig.java
@@ -2,19 +2,30 @@ package com.ahesh.vault.config;
 
 import static hudson.Util.fixEmptyAndTrim;
 
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
 import java.io.Serializable;
+import hudson.model.Item;
+import hudson.security.ACL;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import java.util.List;
 
 public class MyConfig extends AbstractDescribableImpl<MyConfig> implements Serializable {
 
     private String name;
     private StringCredentials password;
+    private String passwordId;
 
     @DataBoundConstructor
     public MyConfig() {}
@@ -23,7 +34,9 @@ public class MyConfig extends AbstractDescribableImpl<MyConfig> implements Seria
         this.name = config.name;
         this.password = config.password;
     }
-
+    public StringCredentials getPassword() {
+        return password;
+    }
     public String getName() {
         return name;
     }
@@ -32,15 +45,15 @@ public class MyConfig extends AbstractDescribableImpl<MyConfig> implements Seria
     public void setName(String name) {
         this.name = fixEmptyAndTrim(name);
     }
-
-    public StringCredentials getPassword() {
-        return password;
-    }
-
     @DataBoundSetter
     public void setPassword(StringCredentials password) {
         this.password = password;
     }
+    @DataBoundSetter
+    public void setPasswordId(String passwordId) {
+        this.passwordId = passwordId;
+    }
+
 
     @Extension
     public static class DescriptorImpl extends Descriptor<MyConfig> {
@@ -49,6 +62,12 @@ public class MyConfig extends AbstractDescribableImpl<MyConfig> implements Seria
         @NonNull
         public String getDisplayName() {
             return "My Config";
+        }
+
+        public ListBoxModel doFillPasswordItems(@AncestorInPath Item item, @QueryParameter String uri) {
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
+            List<DomainRequirement> domainRequirements = URIRequirementBuilder.fromUri(uri).build();
+            return new StandardListBoxModel().includeEmptyValue().includeAs(ACL.SYSTEM2, item, StringCredentials.class, domainRequirements);
         }
     }
 }
